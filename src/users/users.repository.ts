40c14200b176349f0592d46b8267575b32df226c
RAHApc <61222682+RAHApc@users.noreply.github.com>
2022-10-06 17:38:30 +0330

    Update users.repository.ts
    change two

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { User } from './schemas/user.schema';
+import { IUser } from './schemas/user.schema';
 import { FilterQuery, Model, UpdateQuery } from 'mongoose';
 
 @Injectable()
@@ -9,26 +9,26 @@ export class UserRepository {
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
 
-  async findById(id: string): Promise<User> {
-    return this.userModel.findById(id);
+  async findById(id: string): Promise<IUser> {
+    return this.userModel.findById(id).lean();
   }
 
-  async findByEmail(email: string): Promise<User> {
-    return this.userModel.findOne({ email });
+  async findByEmail(email: string): Promise<IUser> {
+    return this.userModel.findOne({ email }).lean();
   }
 
-  async createOne(params: Partial<User>): Promise<User> {
+  async createOne(params: Partial<IUser>): Promise<IUser> {
     const newUser = new this.userModel({ ...params });
     await newUser.save();
     return newUser;
   }
 
   async updateOne(
-    queryFilter: FilterQuery<Partial<User>>,
-    updateData: UpdateQuery<Partial<User>>,
+    queryFilter: FilterQuery<Partial<IUser>>,
+    updateData: UpdateQuery<Partial<IUser>>,
   ) {
     return await this.userModel.findOneAndUpdate(queryFilter, updateData, {
       new: true,
-    });
+    }).lean();
   }
 }
