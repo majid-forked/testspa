10195481358cb131d1006b2ce4fba5c5301dda7f
RAHApc <61222682+RAHApc@users.noreply.github.com>
2022-10-08 11:12:16 +0330

    Update auth.service.ts
    change

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from './../users/users.service';
-import * as bcrypt from 'bcrypt';
 import { comparePassword, hashPassword } from './../services/hash.service';
 import { User } from './../users/schemas/user.schema';
 import { UserRepository } from './../users/users.repository';
@@ -10,7 +9,6 @@ import { AuthUserLoginRequestDto } from './dtos/request/auth-user-login-request.
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userRepository: UserRepository,
     private usersService: UsersService,
   ) {}
 
@@ -18,41 +16,12 @@ export class AuthService {
   async register(user: AuthUserRequestDto) {
     console.log(user);
 
-    const { email, pecuniary, image, password } = user;
-
-    const hashedPassword = hashPassword(password);
-
-    const newUser = await this.userRepository.createOne({
-      email,
-      pecuniary,
-      image,
-      password: hashedPassword,
-    });
-
-    if (!newUser) {
-      throw new Error('user creation failed');
-    }
-
-    return this.usersService.buildUserResponse([newUser, 'success']);
+    return this.usersService.createUser(user);
   }
 
   // login
   async authenticate(data: AuthUserLoginRequestDto): Promise<any> {
-    const { email, password } = data;
-    
-    const user = await this.userRepository.findByEmail(email);
-    if (!user) {
-      throw new Error('user not found');
-    }
-    const passwordCheck = comparePassword(password, user.password);
-
-    if (!passwordCheck) {
-      throw new Error('authentication failed');
-    }
-
-    const usernewBuild = this.usersService.buildUserResponse([user, 'success']);
-    const token = await this.usersService.createJWT(user);
-
-    return { ...usernewBuild, token };
+        
+    return await this.usersService.findByEmailAuth(data);
   }
 }
